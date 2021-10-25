91c9f020da931ef994dfa7ea028e896ff8b17103
claudinei messias <claudineimessias@hotmail.com>
2021-10-25 17:31:15 -0300

    atualização, adicionado tela de login

--- fotos.js
+++ fotos.js
@@ -1,13 +1,13 @@
 let fotosJson = [
-    {id:1, 'src':'./imgG/cursoVideoG.png'},
-    {id:2, 'src':'./imgG/frontboxG.png'},
-    {id:3, 'src':'./imgG/newforceG.png'},
-    {id:4, 'src':'./imgG/netflixG.png'},
+    {id:1, 'src':'./gifs/telaDeLogin.gif'},
+    {id:2, 'src':'./imgG/cursoVideoG.png'},
+    {id:3, 'src':'./imgG/frontboxG.png'},
+    {id:4, 'src':'./imgG/newforceG.png'},
     {id:5, 'src':'./imgG/netflixG.png'},
-    {id:6, 'src':'./imgG/wildbeastG.png'},
+    {id:6, 'src':'./imgG/netflixG.png'},
+    {id:7, 'src':'./imgG/wildbeastG.png'},
     {id:7, 'src':'./imgG/netflixG.png'},
-    {id:8, 'src':'./imgG/netflixG.png'},
-
+    
 ];
 
 
