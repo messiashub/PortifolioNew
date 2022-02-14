783a86bbe23d6125a093bb3ecbe420474ca7e1e2
claudinei messias <claudineimessias@hotmail.com>
2022-02-14 01:12:08 -0300

    update

--- fotos.js
+++ fotos.js
@@ -1,15 +1,14 @@
 let fotosJson = [
 
-    {id:1, 'src':'./gifs/app.finances.gif'},
-    {id:2, 'src':'./gifs/telaDeLogin.gif'},
-    {id:3, 'src':'./imgG/cursoVideoG.png'},
-    {id:4, 'src':'./imgG/frontboxG.png'},
-    {id:5, 'src':'./imgG/newforceG.png'},
-    {id:6, 'src':'./imgG/netflixG.png'},
-    {id:7, 'src':'./imgG/netflixG.png'},
-    {id:8, 'src':'./imgG/wildbeastG.png'},
-    {id:9, 'src':'./imgG/netflixG.png'},
-    
+
+    {id:1, 'src':'./imgG/BarberG.webp'},
+    {id:2, 'src':'./gifs/app.finances.gif'},
+    {id:3, 'src':'./gifs/telaDeLogin.gif'},
+    {id:4, 'src':'./imgG/cursoVideoG.png'},
+    {id:5, 'src':'./imgG/frontboxG.png'},
+    {id:6, 'src':'./imgG/newforceG.png'},
+    {id:7, 'src':'./imgG/netflixG.png'},   
+    {id:8, 'src':'./imgG/wildbeastG.png'},   
 ];
 
 
